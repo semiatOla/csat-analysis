caf37d6a9a5c067c85868022da1df10a8c898ed2
Sémiat Oyénikè OLAITAN <semiat.olaitan.ext@yas.tg>
2026-06-26 08:52:05 +0000

    Requirements update

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def load_sentiment_pipeline():
     sentiment_pipeline = pipeline(
         "sentiment-analysis",
         model="cardiffnlp/twitter-xlm-roberta-base-sentiment",
+        tokenizer="cardiffnlp/twitter-xlm-roberta-base-sentiment",
         device=-1
     )
     bert_pipeline = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
